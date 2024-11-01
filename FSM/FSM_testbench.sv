f90698ce366d420c3b662d290ab38c3c253b23a2
yaduraj7 <YADURAJTOMAR@GMAIL.COM>
2024-11-01 23:06:10 +0530

    FSM_testbench.v

--- FSM/FSM_testbench.sv
+++ FSM/FSM_testbench.sv
@@ -12,26 +12,29 @@
 // 
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ps
 module MUL_test;
 reg [15:0] data_in;
 reg clk, start;
 wire done;
-MUL_datapath DP (eqz, LdA, LdB, LdP, clrP, decB, data_in, clk);
+wire [15:0] Result;
+MUL_datapath DP (eqz, LdA, LdB, LdP, clrP, decB, data_in, clk, Result);
 controller CON (LdA, LdB, LdP, clrP, decB, done, clk, eqz, start);
 initial
 begin
 clk = 1'b0;
 #3 start = 1'b1;
-#500 $finish;
+#5000 $finish;
 end
-always #5 clk = ~clk;initial
+always #5 clk = ~clk;
+initial
 begin
 #17 data_in = 17;
 #10 data_in = 5;
 end
 initial
 begin
-$monitor ($time, " %d %b", DP.Y, done);
-$dumpfile ("mul.vcd"); $dumpvars (0, MUL_test);
+$monitor ($time, " %d %b %d", DP.Y, done, DP.Bout);
+//$dumpfile ("mul.vcd"); $dumpvars (0, MUL_test);
 end
 endmodule
